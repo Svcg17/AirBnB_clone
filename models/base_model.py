bd24d5c284fbddaa8d6f6b5c9c608aeea961f589
Sofia Cheung <659@holbertonschool.com>
2019-06-28 01:53:25 +0000

    Added comments, fixed pep8 errors

--- models/base_model.py
+++ models/base_model.py
@@ -1,36 +1,43 @@
 #!/usr/bin/python3
+""" The base_model module
+"""
 import uuid
 import datetime
 import copy
+
+
 class BaseModel:
-    """class BaseModel that defines all common attr/method"""
+    """class BaseModel that defines all common attr/method
+    """
     def __init__(self, *args, **kwargs):
-        self.updated_at = datetime.datetime.now()
         if kwargs:
-                for keys, value in kwargs.items():
-                    if keys == '__class__':
-                        continue
-                    elif keys == "created_at":
-                        value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    elif keys == "updated_at":
-                        value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')   
-                    setattr(self, keys, value)
+            for keys, value in kwargs.items():
+                if keys == '__class__':
+                    continue
+                elif keys == "created_at" or keys == "updated_at":
+                    formatt = '%Y-%m-%dT%H:%M:%S.%f'
+                    value = datetime.datetime.strptime(value, formatt)
+                setattr(self, keys, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Checks str"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
 
-    def save(self):
-       self.updated_at = datetime.datetime.now()
+        def save(self):
+            """ updates the public instance attributes
+        """
+            self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        
+        """ Returns a dictionary containing all keys/values
+        """
         newdict = copy.deepcopy(self.__dict__)
         newdict["__class__"] = self.__class__.__name__
         newdict['created_at'] = self.created_at.isoformat()
         newdict['updated_at'] = self.updated_at.isoformat()
-
         return (newdict)
