bd9e4a597065624575aaee3ebe0f5de96b523370
Sofia Cheung <659@holbertonschool.com>
2019-06-30 00:56:23 +0000

    Task 7 updates

--- console.py
+++ console.py
@@ -1,28 +1,49 @@
 #!/usr/bin/python3
 """ Cmd line entry point """
 import cmd
-from cmd import Cmd
+import models
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
 
 class HBNBCommand(cmd.Cmd):
     """ Cmd line interpreter """
     prompt = "(hbnb) "
 
-    def do_exit(self, args):
-        """ EOF implementation """
-        return True 
-    
     def emptyline(self):
         """ empty implementation """
         if self.lastcmd:
             self.lastcmd = ""
-            return self.onecmd('\n') 
+            return self.onecmd('\n')
 
     def do_quit(self, args):
-        """Quit command to exit the program 
+        """Quit command to exit the program
         """
-        raise SystemExit
+        return True
 
-    do_EOF = do_exit
+    do_EOF = do_quit
+
+    def do_create(self, args=None):
+        """Creates a new instance of BaseModel, s
+        aves it (to the JSON file) and prints the id"""
+        try:
+            if not args:
+                print("** class name missing **")
+            else:
+                newinstance = args.split()
+                newinstance = eval(args)()
+                newinstance.save()
+                print(newinstance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, args=None):
+        """Prints the string representation of an instance based
+        on the class name and id. Ex: $ show BaseModel 1234-1234-1234"""
+        line = args.split()
+        if line[1] == instance.id:
+            print(instance)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
