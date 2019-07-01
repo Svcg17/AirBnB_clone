f9e3943eea1251ed266ac8ec500411aff2225347
Ken-Mens <kenneth.mensah@gmail.com>
2019-07-01 23:14:28 +0000

    review public class

---
+++ models/review.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""State class"""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """A Review class"""
+    place_id = ''
+    user_id = ''
+    text = ''
