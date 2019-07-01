8de46fd796763f3d9b26f152d33f0aae84b84d4f
Sofia Cheung <659@holbertonschool.com>
2019-07-01 23:54:54 +0000

    Updating console with task 7

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ Cmd line entry point """
 import cmd
 import models
+import json
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
@@ -40,9 +41,99 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, args=None):
         """Prints the string representation of an instance based
         on the class name and id. Ex: $ show BaseModel 1234-1234-1234"""
-        line = args.split()
-        if line[1] == instance.id:
-            print(instance)
+        try:
+            line = args.split()
+            if len(line) == 0:
+                print("** class name missing **")
+            elif len(line) == 1:
+                print("** instance id missing **")
+            else:
+                instance = line[0] + "." + line[1]
+                if instance in models.storage.all():
+                    print(models.storage.all()[instance])
+                else:
+                    print("** no instance found **")
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_destroy(self, args=None):
+        """Deletes an instance based on the class name and id
+        (save the change into the JSON file)."""
+        try:
+            line = args.split()
+            if len(line) == 0:
+                print("** class name missing **")
+            elif len(line) == 1:
+                print("** instance id missing **")
+            else:
+                instance = line[0] + "." + line[1]
+                if instance in models.storage.all():
+                    del models.storage.all()[instance]
+                    models.storage.save()
+                else:
+                    print("** no instance found **")
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_all(self, args=None):
+        """Prints all string representation of all instances
+        based or not on the class name"""
+        try:
+            line = args.split()
+            temp = []
+            for key in models.storage.all():
+                v = models.storage.all()[key]
+                classname = key.split(".")
+                if classname[0] == args or len(args) == 0:
+                    temp.append(str(v))
+                    print(temp)
+                else:
+                    print("** class doesn't exist **")
+        except NameError:
+            print("** class doesn't exist **")
+
+    def _int(self, stri):
+        try:
+            int(stri)
+            return True
+        except ValueError:
+            return False
+
+    def _float(self, stri):
+        try:
+            float(stri)
+            return True
+        except ValueError:
+            return False
+
+    def do_update(self, args=None):
+        """ Updates an instance based on the class na-
+        me and id by adding or updating attribute
+        (save the change into the JSON file)."""
+        try:
+            argz = args.split()
+            if len(argz) == 0:
+                print("** class name missing **")
+            elif len(argz) == 1:
+                print("** instance id missing **")
+            elif len(argz) == 2:
+                print("** attribute name missing **")
+            elif len(argz) == 3:
+                print("** value missing **")
+            else:
+                objs = argz[0] + "." + argz[1]
+                if objs in models.storage.all():
+                    if self._int(argz[3]):
+                        argz[3] = int(argz[3])
+                    elif self._float(argz[3]):
+                        argz[3] = float(argz[3])
+                    if type(argz[3]) == str:
+                        argz[3] = argz[3][1:-1]
+                    setattr(models.storage.all()[objs], argz[2], argz[3])
+                else:
+                    print("** no instance found **")
+        except ValueError:
+            print("** class doesn't exist **")
 
 
 if __name__ == '__main__':
